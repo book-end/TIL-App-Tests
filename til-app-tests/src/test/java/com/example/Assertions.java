439a80b156ba93337cba87f32e77973c42d02dda
book-end <85774845+book-end@users.noreply.github.com>
2023-09-26 01:51:15 -0700

    added technology category num fact test

--- til-app-tests/src/test/java/com/example/Assertions.java
+++ til-app-tests/src/test/java/com/example/Assertions.java
@@ -35,9 +35,11 @@ public class Assertions {
         System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-
         driver.manage().window().maximize();
+
         driver.get("https://til-jade.vercel.app/");
+
+        driver.findElement(By.cssSelector(".btn-all-categories")).click();
 
         int expectedCount = 12;
 
@@ -48,5 +50,26 @@ public class Assertions {
         int actualCount = elements.size();
 
         Assert.assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void TechnologyCategoryNumerOfFacts() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://til-jade.vercel.app/");
+
+        driver.findElement(By.cssSelector("button.btn-category:nth-child(1)")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".fact")));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector(".fact"));
+        int actualCount = elements.size();
+        int expectedCount = 3;
+
+        Assert.assertEquals(actualCount, expectedCount);
+        
     }
 }
