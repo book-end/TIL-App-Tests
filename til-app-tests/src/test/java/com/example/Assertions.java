9ff5add79b9ab12a194b55c806859b8018ad9567
book-end <85774845+book-end@users.noreply.github.com>
2023-09-26 02:15:16 -0700

    added assertion category tests

--- til-app-tests/src/test/java/com/example/Assertions.java
+++ til-app-tests/src/test/java/com/example/Assertions.java
@@ -53,14 +53,14 @@ public class Assertions {
     }
 
     @Test
-    public void TechnologyCategoryNumerOfFacts() {
+    public void TechnologyCategoryNumberOfFacts() {
         System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://til-jade.vercel.app/");
 
-        driver.findElement(By.cssSelector("button.btn-category:nth-child(1)")).click();
+        driver.findElement(By.xpath("//button[contains(text(), 'technology')]")).click();
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".fact")));
@@ -70,6 +70,139 @@ public class Assertions {
         int expectedCount = 3;
 
         Assert.assertEquals(actualCount, expectedCount);
-        
+    }
+
+    @Test
+    public void ScienceCategoryNumerOfFacts() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://til-jade.vercel.app/");
+
+        driver.findElement(By.xpath("//button[contains(text(), 'science')]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".fact")));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector(".fact"));
+        int actualCount = elements.size();
+        int expectedCount = 2;
+
+        Assert.assertEquals(actualCount, expectedCount);
+    }
+
+    @Test
+    public void FinanceCategoryNumberOfFacts() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://til-jade.vercel.app/");
+
+        driver.findElement(By.xpath("//button[contains(text(), 'finance')]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".fact")));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector(".fact"));
+        int actualCount = elements.size();
+        int expectedCount = 1;
+
+        Assert.assertEquals(actualCount, expectedCount);
+    }
+
+    @Test
+    public void SocietyCategoryNumberOfFacts() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://til-jade.vercel.app/");
+
+        driver.findElement(By.xpath("//button[contains(text(), 'society')]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".fact")));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector(".fact"));
+        int actualCount = elements.size();
+        int expectedCount = 2;
+
+        Assert.assertEquals(actualCount, expectedCount);
+    }
+
+    @Test
+    public void EntertainmentCategoryNumberOfFacts() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://til-jade.vercel.app/");
+
+        driver.findElement(By.xpath("//button[contains(text(), 'entertainment')]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".fact")));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector(".fact"));
+        int actualCount = elements.size();
+        int expectedCount = 1;
+
+        Assert.assertEquals(actualCount, expectedCount);
+    }
+
+    @Test
+    public void HealthCategoryNumberOfFacts() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://til-jade.vercel.app/");
+
+        driver.findElement(By.xpath("//button[contains(text(), 'health')]")).click();
+
+        List<WebElement> elements = driver.findElements(By.cssSelector(".fact"));
+        int actualCount = elements.size();
+        int expectedCount = 0;
+
+        Assert.assertEquals(actualCount, expectedCount);
+    }
+
+    @Test
+    public void HistoryCategoryNumberOfFacts() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://til-jade.vercel.app/");
+
+        driver.findElement(By.xpath("//button[contains(text(), 'history')]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".fact")));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector(".fact"));
+        int actualCount = elements.size();
+        int expectedCount = 3;
+
+        Assert.assertEquals(actualCount, expectedCount);
+    }
+
+    @Test
+    public void NewsCategoryNumberOfFacts() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://til-jade.vercel.app/");
+
+        driver.findElement(By.xpath("//button[contains(text(), 'news')]")).click();
+
+        List<WebElement> elements = driver.findElements(By.cssSelector(".fact"));
+        int actualCount = elements.size();
+        int expectedCount = 0;
+
+        Assert.assertEquals(actualCount, expectedCount);
     }
 }
