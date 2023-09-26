68d7b73cb73d8084ef52745dfde29919606efe67
book-end <85774845+book-end@users.noreply.github.com>
2023-09-26 01:36:17 -0700

    added num fact test

--- til-app-tests/src/test/java/com/example/Assertions.java
+++ til-app-tests/src/test/java/com/example/Assertions.java
@@ -1,10 +1,17 @@
 package com.example;
 
 import org.junit.Test;
+import java.util.List;
+import java.time.Duration;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 public class Assertions {
 
@@ -21,5 +28,25 @@ public class Assertions {
         String actualTitle = driver.getTitle();
 
         Assert.assertEquals(expectedTitle, actualTitle);
+    }
+
+    @Test
+    public void AllCategoryNumberOfFacts() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://til-jade.vercel.app/");
+
+        int expectedCount = 12;
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".fact")));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector(".fact"));
+        int actualCount = elements.size();
+
+        Assert.assertEquals(expectedCount, actualCount);
     }
 }
